12912d86d6c8208f59d2bc17988b8ac99ea128bd
kphamgit <kevinpham.us@gmail.com>
2025-11-29 08:47:04 -0500

    m

--- api/models.py
+++ api/models.py
@@ -10,9 +10,10 @@ class Note(models.Model):
     def __str__(self):
         return self.title
 
-#class Category(models.Model):
-#    name = models.CharField(max_length=100)
-
-#    def __str__(self):
-#        return self.name
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    category_number = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
         
