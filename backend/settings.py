8bcc1ad5f5ab8a9beb6144723e7f0793e4409413
kphamgit <kevinpham.us@gmail.com>
2025-12-17 15:55:15 -0500

    m

--- backend/settings.py
+++ backend/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'django_extensions'
+  
 ]
  #'django_extensions', (add only for developmement purposes, ie. , to run show_urls)
 
